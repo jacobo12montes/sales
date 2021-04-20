65cd62bfd963ec48cf59264309951111444ad7f0
jacobo12montes <jacobo12.montes@gmail.com>
2021-04-20 01:42:55 -0500

    Attaching files

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,4 @@
 class Sale < ApplicationRecord
+    has_one_attached :icon
     belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :sales
-  root 'clients#index'
+  root 'sales#index'
   devise_for :users
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
